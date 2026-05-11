0fd29489ec25a1ae60a2d7b3639800182ff98427
andoniainaravalimanana <andoniaina.ravalimanana@gmail.com>
2026-05-11 10:08:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -408,7 +408,8 @@ async function openPdf(file) {
     await renderAllPages(pdf);
 
     state.pdf.currentPage = Math.min(lastPage, pdf.numPages);
-    setTimeout(() => scrollToPage(state.pdf.currentPage);
+    setTimeout(() => {
+    scrollToPage(state.pdf.currentPage);
     setTimeout(() => scrollToPage(state.pdf.currentPage), 800);
     }, 400);
     updatePageDisplay(state.pdf.currentPage);
